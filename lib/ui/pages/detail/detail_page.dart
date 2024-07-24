bb99513f8d8c22f41f2edf470c807952f3fa6a70
huydq <huydq@rocketstudio.com>
2024-07-24 17:22:39 +0700

    add chart

--- lib/ui/pages/detail/detail_page.dart
+++ lib/ui/pages/detail/detail_page.dart
@@ -1,5 +1,8 @@
+import 'package:electronic_data/ui/pages/detail/components/line_chart.dart';
 import 'package:electronic_data/ui/resources/themes/app_text_theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -32,12 +35,117 @@ class _DetailPageState extends State<DetailPage> {
         ),
       ),
       body: SizedBox(
-        child: Column(
-          children: [
-            
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _infoWidget(
+                    title: "ĐIỆN ÁP",
+                    parameter: "205",
+                    textColor: Colors.red,
+                    unit: "V",
+                  ),
+                  _infoWidget(
+                    title: "Dòng điện",
+                    parameter: "0",
+                    textColor: Colors.yellow[700]!,
+                    unit: "A",
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _infoWidget(
+                    title: "Công suất",
+                    parameter: "0",
+                    textColor: Colors.purple,
+                    unit: "W",
+                  ),
+                  _infoWidget(
+                    title: "Điện năng hiện tại",
+                    parameter: "0,104",
+                    textColor: Colors.blue,
+                    unit: "kWh",
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _infoWidget(
+                    title: "Tần số",
+                    parameter: "49,9",
+                    textColor: Colors.black,
+                    unit: "Hz",
+                  ),
+                  _infoWidget(
+                    title: "Điện năng hiện tại",
+                    parameter: "0",
+                    textColor: Colors.orange,
+                    unit: "",
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+              const Text("ĐIỆN NĂNG QUA CÁC NGÀY"),
+              const DataLineCharts()
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _infoWidget({
+    required String title,
+    required String parameter,
+    required String unit,
+    required Color textColor,
+  }) {
+    return Expanded(
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            title.toUpperCase(),
+            style: AppTextTheme.greyS14Bold,
+          ),
+          const SizedBox(height: 20),
+          _buildDataItem(
+            parameter: parameter,
+            unit: unit,
+            textColor: textColor,
+          )
+        ],
+      ),
+    );
+  }
+
+  Row _buildDataItem({
+    required String parameter,
+    required String unit,
+    required Color textColor,
+  }) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          parameter,
+          style: AppTextTheme.blackS24.copyWith(color: textColor),
+        ),
+        Text(
+          unit,
+          style: AppTextTheme.blackS16.copyWith(color: textColor),
+        ),
+      ],
     );
   }
 }
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:electronic_data/ui/pages/detail/detail_page.dart';
 import 'package:electronic_data/ui/resources/themes/app_text_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -20,56 +21,66 @@ class _HomePageState extends State<HomePage> {
           itemCount: 1,
           shrinkWrap: true,
           itemBuilder: (context, index) {
-            return Container(
-              height: 120,
-              padding: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 12,
-              ),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 3,
-                    offset: const Offset(0, 1), // changes position of shadow
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const DetailPage(),
                   ),
-                ],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "PZEM004T",
-                        style: AppTextTheme.blackS14Bold,
-                      ),
-                      const Spacer(),
-                      Container(
-                        height: 8,
-                        width: 8,
-                        decoration: const BoxDecoration(
-                          color: Colors.blue,
-                          shape: BoxShape.circle,
+                );
+              },
+              child: Container(
+                height: 120,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 12,
+                ),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 3,
+                      offset: const Offset(0, 1), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "PZEM004T",
+                          style: AppTextTheme.blackS14Bold,
                         ),
-                      )
-                    ],
-                  ),
-                  const Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      _buildDataItem(parameter: "205", unit: "V"),
-                      _buildDataItem(parameter: "0", unit: "A"),
-                      _buildDataItem(parameter: "0", unit: "W"),
-                    ],
-                  )
-                ],
+                        const Spacer(),
+                        Container(
+                          height: 8,
+                          width: 8,
+                          decoration: const BoxDecoration(
+                            color: Colors.blue,
+                            shape: BoxShape.circle,
+                          ),
+                        )
+                      ],
+                    ),
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buildDataItem(parameter: "205", unit: "V"),
+                        _buildDataItem(parameter: "0", unit: "A"),
+                        _buildDataItem(parameter: "0", unit: "W"),
+                      ],
+                    )
+                  ],
+                ),
               ),
             );
           },
